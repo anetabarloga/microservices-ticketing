75be30961282b2ac502a5d03f3c68df46f1e875e
Aneta Barloga <anetabarloga@gmail.com>
2022-03-21 15:39:18 +0100

    add signup validation

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,8 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-	res.send("Sign up!");
-});
+router.post(
+	"/api/users/signup/",
+	[body("email").isEmail().withMessage("Enter a valid email address"), body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password length must be between 4 and 20 characters")],
+	(req: Request, res: Response) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).send(errors.array());
+		}
+		const { email, password } = req.body;
+
+		console.log("Creating user...");
+		res.send({});
+	}
+);
 
 export { router as signUpRouter };
