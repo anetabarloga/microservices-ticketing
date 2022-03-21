1aca5ed0bbeda8530417e0d5e107e57e4760e476
Aneta Barloga <anetabarloga@gmail.com>
2022-03-21 12:56:33 +0100

    add authorisation api routes

---
+++ auth/src/routes/sign-out.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+
+router.post("/api/users/signout", (req, res) => {
+	res.send("Sign out!");
+});
+
+export { router as signOutRouter };
